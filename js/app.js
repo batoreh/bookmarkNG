690fcdc97519c7e2cf17df2bf9f62b71d52c4560
Neto Guimarães <krusdora@gmail.com>
2016-06-16 01:14:06 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,14 +8,28 @@ ngbookmark.controller('BookmarkControler', function BookmarkControler($scope){
 			url: 'http://g1.globo.com/',
 			img: 'https://pbs.twimg.com/profile_images/652253314210467840/gdCnHsYD_400x400.jpg',
 			name: 'G1'
-		},
-		{
+		}, {
 			id: 'github',
 			url: 'http://github.com/',
 			img: 'bora',
 			name: 'github'
+		}, {
+			id: 'wjblog',
+			url: 'https://willianjusten.com.br/',
+			img: 'bora',
+			name: 'wilian justen blog'
+		}, {
+			id: 'apblog',
+			url: 'http://afonsopacifer.com/',
+			img: 'bora',
+			name: 'Afonso Pacifer blog'
+		}, {
+			id: 'hwblog',
+			url: 'http://www.heydonworks.com/',
+			img: 'bora',
+			name: 'heydonworks blog'
 		}
+
 	];
 
 });
-
